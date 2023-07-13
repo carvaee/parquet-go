18fefa2d6d8d14e6d86e762351948823456a9308
carvaee <369252574@163.com>
2023-07-13 10:46:09 +0800

    Update common.go

--- common/common.go
+++ common/common.go
@@ -11,7 +11,7 @@ import (
 
 	"github.com/apache/arrow/go/arrow"
 	"github.com/apache/arrow/go/arrow/array"
-	"github.com/xitongsys/parquet-go/parquet"
+	"github.com/carvaee/parquet-go/parquet"
 )
 
 // `parquet:"name=Name, type=FIXED_LEN_BYTE_ARRAY, length=12"`

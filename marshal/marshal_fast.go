92466b70a9e8dccfa41cd1df30b28d20ec68a13f
carvaee <369252574@163.com>
2023-07-13 10:54:14 +0800

    Update marshal_fast.go

--- marshal/marshal_fast.go
+++ marshal/marshal_fast.go
@@ -4,11 +4,11 @@ import (
 	"errors"
 	"fmt"
 	"github.com/goccy/go-reflect"
-	"github.com/xitongsys/parquet-go/common"
-	"github.com/xitongsys/parquet-go/layout"
-	"github.com/xitongsys/parquet-go/parquet"
-	"github.com/xitongsys/parquet-go/schema"
-	"github.com/xitongsys/parquet-go/types"
+	"github.com/carvaee/parquet-go/common"
+	"github.com/carvaee/parquet-go/layout"
+	"github.com/carvaee/parquet-go/parquet"
+	"github.com/carvaee/parquet-go/schema"
+	"github.com/carvaee/parquet-go/types"
 	"unsafe"
 )
 

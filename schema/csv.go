f03947c946e4eb739bd07d9808d91f0adf7ac4b6
carvaee <369252574@163.com>
2023-07-13 10:56:54 +0800

    Update csv.go

--- schema/csv.go
+++ schema/csv.go
@@ -3,8 +3,8 @@ package schema
 import (
 	"fmt"
 
-	"github.com/xitongsys/parquet-go/common"
-	"github.com/xitongsys/parquet-go/parquet"
+	"github.com/carvaee/parquet-go/common"
+	"github.com/carvaee/parquet-go/parquet"
 )
 
 //Create a schema handler from CSV metadata
